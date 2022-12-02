9db8e12a19b0773c7e5251e0102662f492953972
Duberney Velez <duberneyvelez11@gmail.com>
2022-12-02 14:02:14 -0500

    Creacion del header- mobile first

--- 5-game-paper/src/App.js
+++ 5-game-paper/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.scss";
+import "./styles/style.scss"
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="App">
-     
+      <Header score ="12"></Header>
     </div>
   );
 }
